0ab7082d52e709e3ed15ce2303e2b471b894d13c
R-abodyak <rawanabodiak12@gmail.com>
2022-03-12 12:32:23 +0200

    PRECEDENCE : update UML diagram

--- priceCalculaterKata/priceCalculaterKata/productFacade.cs
+++ priceCalculaterKata/priceCalculaterKata/productFacade.cs
@@ -28,7 +28,7 @@ class productFacade
     {
 
         return Math.Round(product.Price
-            + FindTax()-calculateTotalDiscount(), 2);
+            + CalculateTax()-calculateTotalDiscount(), 2);
 
     }
     public double calculateTotalDiscount()
@@ -48,7 +48,7 @@ class productFacade
         return false;
 
     }
-    private double FindTax()
+    private double CalculateTax()
     {
         double priceBeforeTax = PriceBeforeTax(calculateDiscountBefore());
 
